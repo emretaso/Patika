6217ea46c51c703bcc17b53cc05ad61c95e3527e
Emre Tasli <76100876+emretaso@users.noreply.github.com>
2022-01-10 21:05:04 +0300

    Update Equivalent Key Presses.py

--- Python_Fundementals/Equivalent Key Presses.py
+++ Python_Fundementals/Equivalent Key Presses.py
@@ -1,5 +1,4 @@
-##Equivalent Key Presses Have the function Equivalent Keypresses(stArr) read the array of strings stored in strArr which will contain 2 strings representing two comma separated lists of keypresses. Your goal is to return the string true if the keypresses produce the same printable string and the string false if they do not. A keypress can be either a printable character or a backspace represented by -B. You can produce a printable string from such a string of keypresses by having backspaces erase one preceding character.
-
+##Equivalent Key Presser
 q = ["", "-B,-B,-B"]
 a,b =  list(map(lambda x: x.split(","), q))
 
